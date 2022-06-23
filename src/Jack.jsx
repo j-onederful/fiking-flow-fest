d7b5e7064155f3a66697f43986b544963d75da0f
“j-onederfulgit config --global user.email “jackwwyman@gmail.com <jackwyman@macbook-pro-2.lan>
2022-06-23 14:37:38 -0700

    turtles

--- src/Jack.jsx
+++ src/Jack.jsx
@@ -4,8 +4,8 @@ export default function Jack() {
   return (
     <div>
         <img src="https://media-exp2.licdn.com/dms/image/C5603AQE9iI0xUwaFdw/profile-displayphoto-shrink_200_200/0/1592253516910?e=1661385600&v=beta&t=2dHv50zkpRWTBCVcyMumUlW_hulw7DRqKIQhOXynPgk"/>
-        <p>Jack</p>
-
+        <h3>Jack</h3>
+        <p>I like toitles</p>
     </div>
   )
 }
