a3e374f3ade0919e72985b5158737e0d585166df
Billy Lu <billyfwlu@gmail.com>
2022-06-23 14:22:39 -0700

    added billy component

---
+++ src/Billy.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Billy() {
+  return (
+    <div>
+        <img src="https://media-exp1.licdn.com/dms/image/C5103AQEqdxgwJIiwEA/profile-displayphoto-shrink_200_200/0/1516606953138?e=1661385600&v=beta&t=jxAsI58EiW5TreNtvekI7R0tVCKy381x_nH_MHfYGYA"/>
+        <p>Billy</p>
+
+    </div>
+  )
+}
