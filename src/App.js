0a7833bf6c026e82d4d4709eb7908a03c1939ac8
“j-onederfulgit config --global user.email “jackwwyman@gmail.com <jackwyman@macbook-pro-2.lan>
2022-06-23 14:34:00 -0700

    added comps

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Wonjune from 'Wonjune'
+import Jack from 'Jack'
+import Billy from 'Billy'
 
 function App() {
   return (
     <div className="App">
       <h1>Codeplay ❄️</h1>
-      <h2>Billy</h2>
-      <h2>Jack</h2>
-      <h2>Wonjune</h2>
+      <Billy />
+      <Jack />
+      <Wonjune />
     </div>
   );
 }
---
+++ src/Jack.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Jack() {
+  return (
+    <div>
+        <img src="https://media-exp2.licdn.com/dms/image/C5603AQE9iI0xUwaFdw/profile-displayphoto-shrink_200_200/0/1592253516910?e=1661385600&v=beta&t=2dHv50zkpRWTBCVcyMumUlW_hulw7DRqKIQhOXynPgk"/>
+        <p>Jack</p>
+
+    </div>
+  )
+}
